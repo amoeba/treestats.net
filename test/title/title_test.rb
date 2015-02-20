4adb3b542f0e0c248da33813a81d5129447f4b51
Bryce Mecum <petridish@gmail.com>
2015-02-20 07:15:07 -0900

    Add a titles test

---
+++ test/title/title_test.rb
@@ -0,0 +1,24 @@
+# Test the /send method with a fake POST
+
+require 'net/http'
+require 'json'
+
+# uri = URI.parse("http://floating-meadow-8649.herokuapp.com/")
+uri = URI.parse("http://localhost:9292")
+
+endpoint = Net::HTTP.new(uri.host, uri.port)
+
+files = Dir["./*.json"]
+
+puts files
+
+exit unless files.length > 0
+
+files.each do |file|
+  request = Net::HTTP::Post.new(uri.request_uri)
+  request.body = File.open(file, 'rb') { |file| file.read }
+
+  response = endpoint.request(request)
+
+  puts response
+end
