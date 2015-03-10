2d8103cee02829082b6abb6f2e7896098446b776
Bryce Mecum <petridish@gmail.com>
2015-03-10 13:19:22 -0800

    Add callbacks and validations on character.rb

--- models/character.rb
+++ models/character.rb
@@ -2,6 +2,9 @@ class Character
   include Mongoid::Document
   include Mongoid::Timestamps::Short
   
+  validates_presence_of :name
+  validates_presence_of :server
+  
   field :n,   as: :name,              type: String
   field :s,   as: :server,            type: String
   field :r,   as: :race,              type: String
@@ -33,4 +36,73 @@ class Character
 
   field :tc,  as: :current_title,     type: Integer
   field :ti,  as: :titles,            type: Array
+  
+  after_save do |document|
+    # Monarch
+    if self.monarch
+      monarch = Character.find_or_create_by(name: self.monarch['name'], server: self.server)
+      
+      if self.allegiance_name
+        monarch.set(allegiance_name: self.allegiance_name)
+      end
+    end
+    
+    # Patron
+    if self.patron
+      patron = Character.find_or_create_by(name: self.patron['name'], server: self.server)
+      
+      patron.set(self.patron)
+      
+      vassals = patron.vassals
+      vassal_record = {
+          'name' => self.name,
+          'rank' => self.rank,
+          'race' => self.race,
+          'gender' => self.gender
+      }
+      v_i = vassals && vassals.find_index { |v| v['name'] == self.name }
+      
+      puts "v_i is #{v_i}"
+      if(v_i) # Detected
+        vassals[v_i] = vassal_record
+      else
+        vassals ||= []
+        vassals.push(vassal_record)
+      end
+      
+      patron.set(vassals: vassals)
+      
+      if self.monarch
+        patron.set(monarch: self.monarch)  
+      end
+      
+      if self.allegiance_name
+        patron.set(allegiance_name: self.allegiance_name)
+      end
+    end
+    
+    # Vassals
+    if self.vassals
+      self.vassals.each do |v|
+        vassal = Character.find_or_create_by(name: v['name'], server: self.server)
+        
+        vassal.set(v)
+
+        vassal.set(patron: { 
+          'name' => self.name,
+          'rank' => self.rank,
+          'race' => self.race,
+          'gender' => self.gender
+          })
+        
+        if self.monarch
+          vassal.set(monarch: self.monarch)  
+        end
+      
+        if self.allegiance_name
+          vassal.set(allegiance_name: self.allegiance_name)
+        end
+      end
+    end
+  end
 end
