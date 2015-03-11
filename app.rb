20f81c89bd0c2e7e4d436d818553a2599db87a04
Bryce Mecum <petridish@gmail.com>
2015-03-11 08:09:41 -0800

    Add force-directed graph layout to allegiance list

--- app.rb
+++ app.rb
@@ -95,7 +95,57 @@ module Treestats
 
       haml :characters
     end
-
+    
+    get '/allegiances/:key.json' do |key|
+      content_type :json
+      
+      @server, @name = key.split("-")
+      
+      @characters = Character.where(server: @server, allegiance_name: @name)
+      
+      nodes = []
+      links = []
+      
+      require 'pp'
+      
+      @characters.each_with_index do |c,i|
+        # Add character as a node if it doesn't exist
+        # Add links
+          # Add characters from links as nodes
+        source_id = nodes.find_index { |n| n[:name] == c.name }
+        
+        if source_id.nil?
+          nodes << { :name => c.name, :group => i } 
+          source_id = nodes.find_index { |n| n[:name] == c.name }
+        end
+        
+        vassal_names = c.vassals.collect { |v| { "name" => v["name"] } } if c.vassals
+        linkages = [c.patron]
+        linkages = linkages.concat(vassal_names) if vassal_names
+        linkages = linkages.collect { |i| i.nil? ? nil : i["name"] }.reject { |i| i.nil? }
+        
+        pp linkages
+        
+        linkages.each do |l|
+          # Find id of source, create otherwise
+          target_id = nodes.find_index { |n| n[:name] == l }
+          nodes << { :name => l, :group => i } if target_id.nil?
+          target_id = nodes.find_index { |n| n[:name] == l }
+          
+          links << { :source => source_id, :target => target_id, :value => 1}
+          
+          # Find id of target, create otherwise
+          target_id = nodes.find_index { |n| n[:name] == l }
+          nodes << { :name => l, :group => i } if target_id.nil?
+          target_id = nodes.find_index { |n| n[:name] == l }
+          
+          links << { :source => source_id, :target => target_id, :value => 1}
+        end
+      end
+      
+      return { "nodes" => nodes, "links" => links }.to_json
+    end
+      
     get '/allegiances/:key' do |key|
       @server, @name = key.split("-")
       
---
+++ public/js/allegtree.js
@@ -0,0 +1,51 @@
+var draw = function(selector, json, options) {
+  var width = options.width || 600,
+      height = options.height || 600;
+
+  var svg = d3.select("#content").append("svg")
+      .attr("width", width)
+      .attr("height", height);
+
+  var force = d3.layout.force()
+      .gravity(.05)
+      .distance(100)
+      .charge(-200)
+      .size([width, height]);
+
+    force
+        .nodes(json.nodes)
+        .links(json.links)
+        .start();
+
+    var link = svg.selectAll(".link")
+        .data(json.links)
+      .enter().append("line")
+        .attr("class", "link");
+
+    var node = svg.selectAll(".node")
+        .data(json.nodes)
+      .enter().append("g")
+        .attr("class", "node")
+        .call(force.drag);
+
+    node.append("a")
+      .attr("xlink:href", function(d) { return ["/", server_name, "/", d.name].join(""); })
+      .append("text")
+        .attr("dx", 12)
+        .attr("dy", ".35em")
+        .text(function(d) { return d.name });
+        
+    node.append("a")
+      .attr("xlink:href", function(d) { return ["/", server_name, "/", d.name].join(""); })
+      .append("circle")
+        .attr("r", 4);
+
+    force.on("tick", function() {
+      link.attr("x1", function(d) { return d.source.x; })
+          .attr("y1", function(d) { return d.source.y; })
+          .attr("x2", function(d) { return d.target.x; })
+          .attr("y2", function(d) { return d.target.y; });
+
+      node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+  });
+}
