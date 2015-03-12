583e99e2794c4ef04282146bd15406db52ccb2f6
Bryce Mecum <petridish@gmail.com>
2015-03-12 13:46:26 -0800

    Handle failed character update
    includes test

--- spec/stories/app_spec.rb
+++ spec/stories/app_spec.rb
@@ -18,6 +18,11 @@ describe "AppStory" do
       Character.all.destroy
     end
           
+    it "reports update failure when the character isn't created" do
+      post('/', '{}')
+      last_response.body.must_equal "Character update failed."
+    end
+    
     it "creates simple characters successfully" do
       Character.count.must_equal 0
       
@@ -26,25 +31,31 @@ describe "AppStory" do
       Character.count.must_equal 1
       
       post('/', '{"name":"test2", "server":"test"}')
+      last_response.body.must_equal "Character was updated successfully."
+      
       post('/', '{"name":"test3", "server":"test"}')
+      last_response.body.must_equal "Character was updated successfully."
       
       Character.count.must_equal 3
     end
     
     it "creates stub vassals" do
       post('/', '{"name":"patron", "server":"test","vassals":[{"name":"testvassal"}]}')
-      
+      last_response.body.must_equal "Character was updated successfully."
+            
       Character.count.must_equal 2
     end
     
     it "creates stub vassals with patron set correctly" do
       post('/', '{"name":"patron", "server":"test","vassals":[{"name":"vassal"}]}')
+      last_response.body.must_equal "Character was updated successfully."
       
       Character.find_by(name: 'vassal').patron['name'].must_equal "patron"
     end
     
     it "assigns monarch correctly" do
       post('/', '{"name":"patron", "server":"test","monarch":{"name":"monarch"},"vassals":[{"name":"vassal"}]}')
+      last_response.body.must_equal "Character was updated successfully."
       
       Character.find_by(name: 'vassal').monarch['name'].must_equal "monarch"
     end
