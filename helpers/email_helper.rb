14c17b71db346ca27d90a8f4794dae21b037c19a
Bryce Mecum <petridish@gmail.com>
2015-03-15 22:28:53 -0800

    Update Email methods to email when a user is updated

--- helpers/email_helper.rb
+++ helpers/email_helper.rb
@@ -2,12 +2,10 @@
 
 module EmailHelper
   def self.send(subject, body)
-    if settings.production?
-      Pony.mail(
-        to: "petridish@gmail.com",
-        from: "petridish@gmail.com",
-        subject: subject,
-        body: body)
-    end
-  end  
+    Pony.mail(
+      to: "petridish@gmail.com",
+      from: "petridish@gmail.com",
+      subject: subject,
+      body: body)
+    end  
 end
