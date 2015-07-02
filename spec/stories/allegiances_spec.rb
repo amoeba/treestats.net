fbbac2ad5b95cb75ad2552b3e730bac7735ff8de
Bryce Mecum <petridish@gmail.com>
2015-07-01 17:49:08 -0800

    Separate out stories

---
+++ spec/stories/allegiances_spec.rb
@@ -0,0 +1,31 @@
+# spec/stories/app_spec.rb
+
+require_relative '../story_helper.rb'
+
+describe "AllegianceStory" do
+  describe "POST / allegiances" do
+    before do
+      Character.all.destroy
+      Allegiance.all.destroy
+    end
+
+    it "doesn't create empty allegiances" do
+      post('/', '{"name":"testname", "server":"testserver"}')
+
+      Allegiance.count.must_equal 0
+    end
+
+    it "creates allegiances" do
+      post('/', '{"name":"testname", "server":"testserver", "allegiance_name":"someallegiance"}')
+
+      Allegiance.count.must_equal 1
+    end
+
+    it "doesn't create duplicate allegiances" do
+      post('/', '{"name":"testname", "server":"testserver", "allegiance_name":"someallegiance"}')
+      post('/', '{"name":"testname", "server":"testserver", "allegiance_name":"someallegiance"}')
+
+      Allegiance.count.must_equal 1
+    end
+  end
+end
