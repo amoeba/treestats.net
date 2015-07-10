79710f1a359f0853ff1cd95f35f38d8076a47d9a
Bryce Mecum <petridish@gmail.com>
2015-07-10 15:13:33 -0800

    Add Rollbar and dotenv

--- Gemfile
+++ Gemfile
@@ -13,3 +13,5 @@ gem 'sinatra-redis'
 group :test do
   gem "rack-test"
 end
+gem 'rollbar', '~> 1.5.3'
+gem 'dotenv', :groups => [:development, :test]
--- app.rb
+++ app.rb
@@ -1,6 +1,11 @@
+Dotenv.load if ENV['RACK_ENV'] == "development"
+
+require 'rollbar'
+
 Dir["./helpers/*.rb"].each { |file| require file }
 Dir["./models/*.rb"].each { |file| require file }
 Dir["./routes/*.rb"].each { |file| require file }
+
 
 class App < Sinatra::Application
   configure do
@@ -8,5 +13,10 @@ class App < Sinatra::Application
 
     # Mongoid
     Mongoid.load!("./config/mongoid.yml")
+
+    # Rollbar
+    Rollbar.configure do |config|
+      config.access_token = ENV["ROLLBAR_ACCESS_TOKEN"]
+    end
   end
 end
