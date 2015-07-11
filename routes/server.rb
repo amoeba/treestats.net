001a17e59080e7cd4a9b80c688788ea80d982e7c
Bryce Mecum <petridish@gmail.com>
2015-07-10 16:07:42 -0800

    Remove unnecessary not_found error

--- routes/server.rb
+++ routes/server.rb
@@ -6,8 +6,6 @@ class App < Sinatra::Application
   get '/:server/?' do |server|
     @characters = Character.where(server: server).limit(100).desc(:updated_at).where(:attribs.exists => true)
 
-    raise Sinatra::NotFound if !@characters
-
     haml :server
   end
 end
