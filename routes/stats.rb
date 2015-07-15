28c5eda8f8b2966854dc17dfd846cb442fbe07c7
Bryce Mecum <petridish@gmail.com>
2015-07-14 19:18:47 -0800

    Add monthly upload stats controller

--- routes/stats.rb
+++ routes/stats.rb
@@ -6,4 +6,12 @@ class App < Sinatra::Application
 
     result.to_json
   end
+  
+  get '/stats/uploads/monthly' do
+    value = redis.keys "uploads.monthly.*"
+
+    result = value.sort { |a,b| a <=> b }.map { |v| { :date => v.split(".")[2], :count => redis.get(v).to_i }}
+
+    result.to_json
+  end
 end
