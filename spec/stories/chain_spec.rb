7af494abd43b083041b232336eca70c78246fff9
Bryce Mecum <petridish@gmail.com>
2015-07-14 19:25:20 -0800

    Add chain generation spec and clean up specs

---
+++ spec/stories/chain_spec.rb
@@ -0,0 +1,61 @@
+# spec/stories/tree_spec
+
+describe "ChainStory" do
+  before do
+    Character.all.destroy
+  end
+
+  it "can generate a simple chain" do
+    Character.create({"name" => 'some patron', "server" => "test", "vassals" => [{"name" => "vassalA"}, {"name" => "vassalB"}, {"name" => "vassalC"}]})
+    Chain.new("test", "some patron").get_chain.must_equal({"name"=>"some patron", "children"=>[{"name"=>"vassalA"}, {"name"=>"vassalB"}, {"name"=>"vassalC"}]})
+  end
+
+  it "connects distance relationships" do
+    super_patron = Character.create({
+      "name" => "Mr Adventure",
+      "server" => "testserver",
+      "vassals" => [ {
+        "name" => "Pew the Mottled"
+        }]
+      })
+
+    character = Character.create({
+      "name" => "Barbados",
+      "server" => "testserver",
+      "patron" => { "name" => "Pew the Mottled"}
+      })
+
+    chain = Chain.new("testserver", "Barbados").get_chain
+    chain.must_equal({"name"=>"Mr Adventure", "children"=>[{"name"=>"Pew the Mottled", "children"=>[{"name"=>"Barbados"}]}]})
+  end
+
+  it "connects distance relationships when characters are updated in the other order" do
+    character = Character.create({
+      "name" => "Barbados",
+      "server" => "testserver",
+      "patron" => { "name" => "Pew the Mottled"}
+      })
+
+
+    super_patron = Character.create({
+      "name" => "Mr Adventure",
+      "server" => "testserver",
+      "vassals" => [ {
+        "name" => "Pew the Mottled"
+        }]
+      })
+
+    chain = Chain.new("testserver", "Barbados").get_chain
+    chain.must_equal({"name"=>"Mr Adventure", "children"=>[{"name"=>"Pew the Mottled", "children"=>[{"name"=>"Barbados"}]}]})
+  end
+
+  it "connects distance relationships when characters when uploaded as JSON" do
+    post('/', '{"name":"Mr Adventure", "server":"test", "vassals":[{"name":"Pew the Mottled"}]}')
+    post('/', '{"name":"Barbados", "server":"test", "patron":{"name":"Pew the Mottled"}}')
+
+    Character.count.must_equal 3
+
+    chain = Chain.new("test", "Barbados").get_chain
+    chain.must_equal({"name"=>"Mr Adventure", "children"=>[{"name"=>"Pew the Mottled", "children"=>[{"name"=>"Barbados"}]}]})
+  end
+end
