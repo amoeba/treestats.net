354c78e72a4cf30b728db9b9e0b8819cf753517b
Bryce Mecum <petridish@gmail.com>
2015-07-14 19:21:39 -0800

    Temporarily? add wildcard routes to a later-run route

--- routes/zzz.rb
+++ routes/zzz.rb
@@ -1,10 +1,4 @@
 class App < Sinatra::Application
-  get '/characters/?' do
-    @characters = Character.all.limit(100).desc(:updated_at).where(:attribs.exists => true)
-
-    haml :characters
-  end
-
   get '/:server/:name.json' do |s,n|
     begin
       @character = Character.find_by(server: s, name: n)
@@ -12,10 +6,8 @@ class App < Sinatra::Application
       not_found
     end
 
-    @character = @character.as_document.tap {|h| h.delete("_id")}
-
     content_type 'application/json'
-    JSON.pretty_generate(@character)
+    JSON.pretty_generate(@character.serializable_hash({}).tap {|h| h.delete("id")})
   end
 
   get '/:server/:name/?' do |s,n|
@@ -26,5 +18,5 @@ class App < Sinatra::Application
     end
 
     haml :character
-  end
+  end  
 end
