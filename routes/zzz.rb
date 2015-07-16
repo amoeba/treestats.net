0b7251f7806d0bb9722c4914ceb6d1d9b23f6983
Bryce Mecum <petridish@gmail.com>
2015-07-16 07:04:16 -0800

    Uncomment wildcard routes

--- routes/zzz.rb
+++ routes/zzz.rb
@@ -1,22 +1,22 @@
 class App < Sinatra::Application
-  # get '/:server/:name.json' do |s,n|
-  #   begin
-  #     @character = Character.find_by(server: s, name: n)
-  #   rescue Mongoid::Errors::DocumentNotFound
-  #     not_found
-  #   end
+  get '/:server/:name.json' do |s,n|
+    begin
+      @character = Character.find_by(server: s, name: n)
+    rescue Mongoid::Errors::DocumentNotFound
+      not_found
+    end
 
-  #   content_type 'application/json'
-  #   JSON.pretty_generate(@character.serializable_hash({}).tap {|h| h.delete("id")})
-  # end
+    content_type 'application/json'
+    JSON.pretty_generate(@character.serializable_hash({}).tap {|h| h.delete("id")})
+  end
 
-  # get '/:server/:name/?' do |s,n|
-  #   begin
-  #     @character = Character.find_by(server: s, name: n)
-  #   rescue Mongoid::Errors::DocumentNotFound
-  #     not_found
-  #   end
+  get '/:server/:name/?' do |s,n|
+    begin
+      @character = Character.find_by(server: s, name: n)
+    rescue Mongoid::Errors::DocumentNotFound
+      not_found
+    end
 
-  #   haml :character
-  # end  
+    haml :character
+  end  
 end
