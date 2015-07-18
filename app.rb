48a9285ce9b3dcc1262d44201d4265c2561d86b5
Bryce Mecum <petridish@gmail.com>
2015-07-17 23:28:09 -0800

    Fix redis stats in the sort+map part of the requests too

--- app.rb
+++ app.rb
@@ -424,7 +424,7 @@ end
 get '/stats/uploads/daily' do
   value = redis.keys "uploads:daily:*"
 
-  result = value.sort { |a,b| a <=> b }.map { |v| { :date => v.split(".")[2], :count => redis.get(v).to_i }}
+  result = value.sort { |a,b| a <=> b }.map { |v| { :date => v.split(":")[2], :count => redis.get(v).to_i }}
 
   result.to_json
 end
@@ -432,7 +432,7 @@ end
 get '/stats/uploads/monthly' do
   value = redis.keys "uploads:monthly:*"
 
-  result = value.sort { |a,b| a <=> b }.map { |v| { :date => v.split(".")[2], :count => redis.get(v).to_i }}
+  result = value.sort { |a,b| a <=> b }.map { |v| { :date => v.split(":")[2], :count => redis.get(v).to_i }}
 
   result.to_json
 end
