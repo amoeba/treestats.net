0638b1b1de1e78742dfbc9ca5c26fccc9b8aff7e
Bryce Mecum <petridish@gmail.com>
2015-08-16 22:54:11 -0800

    Change chain algorithm to an iterative one with a max iteration

--- helpers/chain_helper.rb
+++ helpers/chain_helper.rb
@@ -8,10 +8,7 @@ class Chain
     highest_patron = find_highest_patron
     return "{}" if highest_patron.nil?
 
-    tree = {'name' => highest_patron}
-    walk_chain(tree)
-
-    tree
+    walk_chain_it(highest_patron)
   end
 
   def find_highest_patron
@@ -36,6 +33,7 @@ class Chain
 
     while patron
       limit -= 1
+
       highest_patron = patron.name
 
       return highest_patron if limit <= 0
@@ -54,14 +52,56 @@ class Chain
     character = Character.find_by(server: @server, name: current['name'])
 
     return if character.nil?
+    return unless character['vassals']
+    return if level > 200
 
-    if(character['vassals'])
-      current.merge!({'children' => []})
+    current.merge!('children' => [])
 
-      character['vassals'].each_with_index do |vassal, i|
-        current['children'] << { 'name' => vassal['name'] }
-        walk_chain(current['children'][i], level + 1) unless level > 200
+    character['vassals'].each_with_index do |vassal, i|
+      current['children'] << { 'name' => vassal['name'] }
+      puts "Walking chain to #{current['children'][i]} at level #{level}"
+      walk_chain(current['children'][i], level + 1)
+    end
+  end
+
+  def walk_chain_it(start)
+    tree = { 'name' => start, 'children' => nil }
+    cursors = [tree]
+
+    max_it = 500
+
+    while max_it > 0 && cursors.length > 0
+      if cursors.last.key?('children') && cursors.last['children'].nil?
+        record = Character.find_by(server: @server, name: cursors.last['name'])
+
+        if record['vassals'].nil? || record['vassals'].length == 0
+          cursors.last.reject! { |k, v| k == 'children' }
+        else
+          cursors.last['children'] = record['vassals'].map { |v| { 'name' => v['name'], 'children' => nil } }
+        end
+
       end
+
+      if cursors.last['children']
+        next_record = cursors.last['children'].find { |v| v.key?('children') && v['children'].nil? }
+      else
+        next_record = nil
+      end
+
+      if next_record
+        idx = cursors.last['children'].map { |v| v['name'] }.find_index(next_record['name'])
+        cursors << cursors.last['children'][idx]
+      else
+        cursors.pop
+      end
+
+      max_it -= 1
+    end
+
+    if max_it == 0
+      { 'name' => 'Loop found in tree. An invalid patron/vassal relationship exists in this tree so the tree was not generated.'}
+    else
+      tree
     end
   end
 end
