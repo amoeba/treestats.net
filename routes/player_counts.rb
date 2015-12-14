878cbed84c398a44a19850c218d88c92aa613720
Bryce Mecum <petridish@gmail.com>
2015-12-13 23:02:04 -0900

    Add latest pop counts to player count chart

--- routes/player_counts.rb
+++ routes/player_counts.rb
@@ -12,6 +12,7 @@ module Sinatra
           app.get '/player_counts.json' do
             content_type :json
 
+            # Query for the historical data
             keys = redis.keys("pc:mean:*").sort
             result = {}
 
@@ -24,6 +25,17 @@ module Sinatra
 
               result[server] ||= {}
               result[server][date] = redis.get(key)
+            end
+
+            # Grab the latest counts from the raw data
+            servers = %w[Darktide Frostfell Harvestgain Leafcull Morningthaw Thistledown Solclaim Verdantine WintersEbb]
+            today = Time.now.utc.strftime("%Y%m%d")
+
+            servers.each do |server|
+              next unless result.has_key?(server.downcase)
+
+              latest = PlayerCount.where(server: server).desc(:created_at).limit(1)
+              result[server.downcase][today] = latest.to_a.first['c']
             end
 
             result.to_json
