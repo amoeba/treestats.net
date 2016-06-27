49f1cf26bef14ef903ca3e84b49560ef066bb1af
Bryce Mecum <petridish@gmail.com>
2016-06-26 20:33:35 -0800

    Don't plot latest counts on graph

--- routes/player_counts.rb
+++ routes/player_counts.rb
@@ -27,23 +27,12 @@ module Sinatra
               result[server][date] = redis.get(key)
             end
 
-            # Grab the latest counts from the raw data
-            servers = %w[Darktide Frostfell Harvestgain Leafcull Morningthaw Thistledown Solclaim Verdantine WintersEbb]
-            today = Time.now.utc.strftime("%Y%m%d")
-
-            servers.each do |server|
-              next unless result.has_key?(server.downcase)
-
-              latest = PlayerCount.where(server: server).desc(:created_at).limit(1)
-              result[server.downcase][today] = latest.to_a.first['c']
-            end
-
             result.to_json
           end
 
           app.get '/player_counts-latest.json' do
             content_type :json
-            
+
             result = []
 
             servers = %w[Darktide Frostfell Harvestgain Leafcull Morningthaw Thistledown Solclaim Verdantine WintersEbb]
