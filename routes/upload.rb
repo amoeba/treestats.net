1ac7e5cde14310256c2dfbc67c42119e28cfe86e
Bryce Mecum <petridish@gmail.com>
2017-07-10 21:50:33 -0700

    Temp debug logging statement

--- routes/upload.rb
+++ routes/upload.rb
@@ -22,7 +22,7 @@ module Sinatra
             # PARSE
             # Parse message
             json_text = JSON.parse(text)
-
+            print(json_text)
             # Remove verification key if it exists
             if (json_text.has_key?("key"))
               json_text = json_text.tap { |h| h.delete("key") }
