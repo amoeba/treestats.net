fb120e9b0c7fe579a5152b49eede6c57921f66f3
Bryce Mecum <petridish@gmail.com>
2019-03-18 23:35:26 -0800

    Move push script out of rake and into shell

---
+++ push_to_github.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+change_branch=$(git branch filtered)
+echo $change_branch
+
+filter_branch=$(git filter-branch --index-filter 'git rm --cached --ignore-unmatch helpers/encryption_helper.rb' -f filtered)
+echo $filter_branch
+
+git_push=$(git push --force origin filtered:master)
+echo $git_push
+
+delete_branch=$(git branch -D filtered)
+echo $delete_branch
