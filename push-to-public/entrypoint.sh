fedaf63ba190042753e5d93acdd9f8e64b1470d2
Bryce Mecum <petridish@gmail.com>
2019-08-08 22:54:51 -0800

    Try to set up an SSH key

--- push-to-public/entrypoint.sh
+++ push-to-public/entrypoint.sh
@@ -3,8 +3,23 @@
 set -e 
 set -u 
 
+HOST="github.com"
+SSH_PATH="$HOME/.ssh"
 BRANCH="filtered"
 
+# Set up SSH
+# Copied from github.com/maddox/actions
+mkdir -p "$SSH_PATH"
+touch "$SSH_PATH/known_hosts"
+echo "$SSH" > "$SSH_PATH/deploy_key"
+chmod 700 "$SSH_PATH"
+chmod 600 "$SSH_PATH/known_hosts"
+chmod 600 "$SSH_PATH/deploy_key"
+eval $(ssh-agent)
+ssh-add "$SSH_PATH/deploy_key"
+ssh-keyscan -t rsa $HOST >> "$SSH_PATH/known_hosts"
+
+# Do the deploy
 git branch "$BRANCH" && \
   git filter-branch --index-filter 'git rm --cached --ignore-unmatch helpers/encryption_helper.rb' -f "$BRANCH" && \
   git push --force git@github.com:amoeba/treestats.net.git $BRANCH:master && \
