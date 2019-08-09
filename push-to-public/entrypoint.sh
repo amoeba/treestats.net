a7863e0e4b6c8bf2439cdab07f30a57985ad92c5
Bryce Mecum <petridish@gmail.com>
2019-08-08 23:11:31 -0800

    Rewrite action

--- push-to-public/entrypoint.sh
+++ push-to-public/entrypoint.sh
@@ -3,24 +3,33 @@
 set -e 
 set -u 
 
-HOST="github.com"
 SSH_PATH="$HOME/.ssh"
 BRANCH="filtered"
 
 # Set up SSH
-# Copied from github.com/maddox/actions
+# From https://github.com/maxheld83/rsync/blob/master/entrypoint.sh
+printf -- "Giving container SSH abilities..."
+
 mkdir -p "$SSH_PATH"
 touch "$SSH_PATH/known_hosts"
-echo "$SSH" > "$SSH_PATH/deploy_key"
+echo "$FINGERPRINT" > "$SSH_PATH/known_hosts"
+
+echo "$SSH_PRIVATE_KEY" > "$SSH_PATH/deploy_key"
+echo "$SSH_PUBLIC_KEY" > "$SSH_PATH/deploy_key.pub"
+
 chmod 700 "$SSH_PATH"
 chmod 600 "$SSH_PATH/known_hosts"
 chmod 600 "$SSH_PATH/deploy_key"
-eval $(ssh-agent)
+chmod 600 "$SSH_PATH/deploy_key.pub"
+
+eval "$(ssh-agent -s)"
 ssh-add "$SSH_PATH/deploy_key"
-ssh-keyscan -t rsa $HOST >> "$SSH_PATH/known_hosts"
 
 # Do the deploy
+printf -- "Filtering..."
 git branch "$BRANCH" && \
   git filter-branch --index-filter 'git rm --cached --ignore-unmatch helpers/encryption_helper.rb' -f "$BRANCH" && \
   git push --force git@github.com:amoeba/treestats.net.git $BRANCH:master && \
   git branch -D $BRANCH
+
+printf -- "Done"
