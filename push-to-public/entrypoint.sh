3e66914f25ad25014780acd8e2e3bb632dbf240c
Bryce Mecum <petridish@gmail.com>
2019-08-08 19:53:11 -0800

    Update push script

--- push-to-public/entrypoint.sh
+++ push-to-public/entrypoint.sh
@@ -3,4 +3,9 @@
 set -e 
 set -u 
 
-git status
+BRANCH="filtered"
+
+git branch "$BRANCH" && \
+  git filter-branch --index-filter 'git rm --cached --ignore-unmatch helpers/encryption_helper.rb' -f "$BRANCH" && \
+  git push --force github-public $BRANCH:master && \
+  git branch -D $BRANCH
