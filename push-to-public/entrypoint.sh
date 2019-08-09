2859427b79158b1ee0c645d2f4ad47f1d0937425
Bryce Mecum <petridish@gmail.com>
2019-08-08 19:55:06 -0800

    Fix script push remote

--- push-to-public/entrypoint.sh
+++ push-to-public/entrypoint.sh
@@ -7,5 +7,5 @@ BRANCH="filtered"
 
 git branch "$BRANCH" && \
   git filter-branch --index-filter 'git rm --cached --ignore-unmatch helpers/encryption_helper.rb' -f "$BRANCH" && \
-  git push --force github-public $BRANCH:master && \
+  git push --force git@github.com:amoeba/treestats.net.git $BRANCH:master && \
   git branch -D $BRANCH
