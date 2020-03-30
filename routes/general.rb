6e8b1c82df2b5e08f079d104056c1dd24a587cc9
Bryce Mecum <petridish@gmail.com>
2020-03-30 01:33:34 -0800

    Add in total uploads count to index page

--- routes/general.rb
+++ routes/general.rb
@@ -1,22 +1,28 @@
 module Sinatra
   module TreeStats
     module Routing
-      module General        
+      module General
         def self.registered(app)
           app.get '/' do
+            # Total uploads
+            @total_uploads = redis
+              .keys("uploads:daily:*")
+              .collect { |k| redis.get(k) }
+              .reduce(:+)
+
             # Latest counts
             if !redis.exists("dashboard-latest-counts")
               @latest_counts = []
             else
               @latest_counts = Marshal.restore(redis.get("dashboard-latest-counts"))
-            end   
+            end
 
             # Total Uploaded
             if !redis.exists("dashboard-total-uploaded")
               @total_uploaded = []
             else
               @total_uploaded = Marshal.restore(redis.get("dashboard-total-uploaded"))
-            end   
+            end
 
             # Latest uploads
             @latest = Character.desc(:updated_at)
