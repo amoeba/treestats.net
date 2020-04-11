355929141aaa9d0618013c69631dedf3f8fac26e
Bryce Mecum <petridish@gmail.com>
2020-04-10 20:46:58 -0800

    Make quests an Array instead of Hash, duh

--- models/character.rb
+++ models/character.rb
@@ -59,7 +59,7 @@ class Character
 
   field :ar,  as: :archived,          type: Boolean, default: -> { false }
 
-  field :q, as: :quests, type: Hash
+  field :q,   as: :quests,            type: Array
 
   after_save do |document|
     self_race = RaceHelper::get_race_name(self.race.to_i)
