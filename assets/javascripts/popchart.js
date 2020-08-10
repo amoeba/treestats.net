6fda953d2f4eaec3f3580b238f41b5b0df68c76a
Bryce Mecum <petridish@gmail.com>
2020-08-10 12:23:11 -0800

    Prettify popchart.js

--- assets/javascripts/popchart.js
+++ assets/javascripts/popchart.js
@@ -1,41 +1,40 @@
-var popchart = function(selector, data_url)
-{
+var popchart = function (selector, data_url) {
   // Following code copied then adapted from http://bl.ocks.org/mbostock/3884955
 
   // Setup
-  var margin = {top: 20, right: 120, bottom: 30, left: 50},
-      width = 960 - margin.left - margin.right,
-      height = 500 - margin.top - margin.bottom;
+  var margin = { top: 20, right: 120, bottom: 30, left: 50 },
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
   var parseDate = d3.time.format("%Y%m%d").parse;
-  var capitalize = function(s) { return s[0].toUpperCase() + s.slice(1); }
+  var capitalize = function (s) { return s[0].toUpperCase() + s.slice(1); }
 
   var x = d3.time.scale()
-      .range([0, width]);
+    .range([0, width]);
 
   var y = d3.scale.linear()
-      .range([height, 0]);
+    .range([height, 0]);
 
   var color = d3.scale.category10();
 
   var xAxis = d3.svg.axis()
-      .scale(x)
-      .orient("bottom");
+    .scale(x)
+    .orient("bottom");
 
   var yAxis = d3.svg.axis()
-      .scale(y)
-      .orient("left");
+    .scale(y)
+    .orient("left");
 
   var line = d3.svg.line()
-      .interpolate("linear")
-      .x(function(d) { return x(d.date); })
-      .y(function(d) { return y(d.count); });
+    .interpolate("linear")
+    .x(function (d) { return x(d.date); })
+    .y(function (d) { return y(d.count); });
 
   var svg = d3.select(selector).append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
     .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
   // Loading indicator
   svg.append("text")
@@ -52,10 +51,10 @@ var popchart = function(selector, data_url)
     color.domain(d3.keys(data))
 
     // Re-structure and parse values
-    var servers = color.domain().map(function(name) {
+    var servers = color.domain().map(function (name) {
       return {
         name: name,
-        values: data[name].map(function(count) {
+        values: data[name].map(function (count) {
           return { date: parseDate(count.date), count: +count.count };
         })
       };
@@ -64,14 +63,14 @@ var popchart = function(selector, data_url)
     // Find the min and max values for the scales
     // TODO: Consider a more efficient way to do this
     var xvals = [],
-        yvals = [];
+      yvals = [];
 
-    servers.forEach(function(server) {
-      var dates = server.values.map(function(data) {
+    servers.forEach(function (server) {
+      var dates = server.values.map(function (data) {
         return data.date
       });
 
-      var counts = server.values.map(function(data) {
+      var counts = server.values.map(function (data) {
         return data.count
       });
 
@@ -83,37 +82,37 @@ var popchart = function(selector, data_url)
     y.domain([0, d3.max(yvals) * 1.25]);
 
     svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
 
     svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
+      .attr("class", "y axis")
+      .call(yAxis)
       .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("Players");
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Players");
 
     var server = svg.selectAll(".server")
-        .data(servers)
+      .data(servers)
       .enter().append("g")
-        .attr("class", "servers");
+      .attr("class", "servers");
 
     server.append("path")
-        .attr("class", "line")
-        .attr("d", function(d) { return line(d.values); })
-        .style("stroke", function(d) { return color(d.name); });
+      .attr("class", "line")
+      .attr("d", function (d) { return line(d.values); })
+      .style("stroke", function (d) { return color(d.name); });
 
     server.append("text")
-        .datum(function(d) { return {name: d.name, value: d.values[d.values.length - 1]}; })
-        .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.count) + ")"; })
-        .attr("x", 3)
-        .attr("dy", ".35em")
-        .attr("class", "label")
-        .text(function(d) { return capitalize(d.name) + ": " + Math.round(d.value.count)})
-        .style("fill", function(d) { return color(d.name); });
+      .datum(function (d) { return { name: d.name, value: d.values[d.values.length - 1] }; })
+      .attr("transform", function (d) { return "translate(" + x(d.value.date) + "," + y(d.value.count) + ")"; })
+      .attr("x", 3)
+      .attr("dy", ".35em")
+      .attr("class", "label")
+      .text(function (d) { return capitalize(d.name) + ": " + Math.round(d.value.count) })
+      .style("fill", function (d) { return color(d.name); });
   });
 }
