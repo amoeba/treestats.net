79706c6808ecdbb3261c8f3d52a9452c8f31a507
Bryce Mecum <petridish@gmail.com>
2023-02-20 20:27:11 -0900

    Turn off Sentry performance monitoring

--- app.rb
+++ app.rb
@@ -4,8 +4,6 @@ if ENV["RACK_ENV"] == "production"
 
   Sentry.init do |config|
     config.dsn = ENV["SENTRY_DSN"]
-    config.breadcrumbs_logger = [:sentry_logger, :http_logger]
-    config.traces_sample_rate = 0.2
   end
 end
 
