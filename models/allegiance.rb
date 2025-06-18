184790f72be8bc259c961f27a5e0fd8550bfc800
Galli <99272482+KT053374@users.noreply.github.com>
2025-06-18 13:45:26 -0400

    Fix whitespace issues

--- models/allegiance.rb
+++ models/allegiance.rb
@@ -1,10 +1,10 @@
 class Allegiance
   include Mongoid::Document
   include Mongoid::Timestamps::Short
-  
+
   validates_presence_of :server
   validates_presence_of :name
-  
+
   field :s,   as: :server,  type: String
   field :n,   as: :name,    type: String
 end
--- models/log.rb
+++ models/log.rb
@@ -1,7 +1,7 @@
 class Log
   include Mongoid::Document
   include Mongoid::Timestamps::Created::Short
-  
+
   field :t, as: :title, type: String
   field :m, as: :message,  type: String
 end
