8fbab2a9e6d2b3b82e0687b864b8fd00cdf0785b
Bryce Mecum <petridish@gmail.com>
2026-04-28 23:26:02 -0700

    add test for new query logic

--- spec/unit/player_counts_helper_spec.rb
+++ spec/unit/player_counts_helper_spec.rb
@@ -1,7 +1,32 @@
 require_relative '../spec_helper'
 
 describe 'PlayerCountsHelper', :unit do
-  it "can do some queries" do
-    refute_empty player_counts
+  before do
+    PlayerCount.all.destroy
+  end
+
+  describe "date boundary" do
+    before do
+      now_utc = Time.now.utc
+      @today_midnight_utc     = Time.utc(now_utc.year, now_utc.month, now_utc.day)
+      @yesterday_midnight_utc = @today_midnight_utc - 86400
+
+      # Use the collection API to set c_at explicitly, bypassing the Mongoid
+      # timestamp callback which would overwrite it with Time.now.
+      PlayerCount.collection.insert_one({ s: "TestServer", c: 100, c_at: @yesterday_midnight_utc + 3600 })
+      PlayerCount.collection.insert_one({ s: "TestServer", c: 200, c_at: @today_midnight_utc   + 3600 })
+    end
+
+    it "excludes today's data" do
+      result = JSON.parse(player_counts(["TestServer"], "All"))
+      today_str = @today_midnight_utc.strftime("%Y%m%d")
+      assert result.none? { |r| r["date"] == today_str }, "today's data should not appear"
+    end
+
+    it "includes yesterday's data" do
+      result = JSON.parse(player_counts(["TestServer"], "All"))
+      yesterday_str = @yesterday_midnight_utc.strftime("%Y%m%d")
+      refute_empty result.select { |r| r["date"] == yesterday_str }, "yesterday's data should appear"
+    end
   end
 end
